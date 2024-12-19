134f8a185e7c63e694b6fc86e97907abda7c5485
jn4varro <jnavarro.artist@gmail.com>
2024-12-19 18:35:07 +0100

    reconfiguracion

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,9 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+/// <reference types="astro/client" />
+declare module '*.jpg';
+declare module '*.png';
+declare module '*.jpeg';
+declare module '*.gif';
+declare module '*.svg';
+declare module '*.PNG';
